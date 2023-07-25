3e5864a1c506f0fe7ba16c2010e1c9274a4f1712
dzoric1 <Gerasimov2099@mail.ru>
2023-07-25 13:55:20 +0800

    add cors middleware

--- backend/utils/variables.js
+++ backend/utils/variables.js
@@ -5,6 +5,16 @@ const limiterSettings = {
   legacyHeaders: false,
 };
 
+const allowedCors = [
+  'http://mesto.dzoric1.nomoredomains.xyz/',
+  'https://mesto.dzoric1.nomoredomains.xyz/',
+  'http://localhost:3000',
+];
+
+const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
 export {
   limiterSettings,
+  allowedCors,
+  DEFAULT_ALLOWED_METHODS,
 };
