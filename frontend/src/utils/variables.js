677a42fecdd9255ed60472bdeecfc9f577f680b3
dzoric1 <Gerasimov2099@mail.ru>
2023-07-25 13:59:13 +0800

    fix api link

--- frontend/src/utils/variables.js
+++ frontend/src/utils/variables.js
@@ -1,15 +1,15 @@
 export const
   apiSettings = {
-    baseUrl: 'http://localhost:3001/',
+    baseUrl: 'http://api.mesto.dzoric1.nomoredomains.xyz/',
     headers: {
-      authorization: '20215c55-16b8-4e5b-8b47-0ed8b95a6c7d',
+      // authorization: '20215c55-16b8-4e5b-8b47-0ed8b95a6c7d',
       'Content-Type': 'application/json'
     }
   }
 
 export const
   authSettings = {
-    baseUrl: 'http://localhost:3001/',
+    baseUrl: 'http://api.mesto.dzoric1.nomoredomains.xyz/',
     headers: {
       'Content-Type': 'application/json'
     }
